abe76b09fed7331a617fc025d8f48e7f7cd6dc3b
Bishop Lafer <bishoplafer@Bishops-MacBook-Pro.local>
2016-02-14 01:42:42 -0800

    success

---
+++ communityActionApp/app/components/Success.js
@@ -0,0 +1,92 @@
+var React = require('react-native');
+var Success = require('./IssueType');
+
+var {
+    TouchableHighlight,
+    Text,
+    StyleSheet,
+    View
+} = React;
+
+class SnapPicture extends React.Component{
+    handleSubmit(event){
+      var report = this.props.report;
+      report.picture = 0;
+
+      this.setState({
+        report: report
+      });
+      this.props.navigator.push({
+        title: 'Report an Issue',
+        component: IssueType,
+        passProps: {report: this.state.report}
+      });
+      this.setState({
+        report: ''
+      });
+    }
+    render(){
+        var layout = (
+        <View style={styles.container}>
+          <View style={styles.successContainer}>
+            <View style={styles.successHeader}>
+              <Text style={styles.successType}>
+                Report Submitted Successfully
+              </Text>
+            </View>
+            <TouchableHighlight
+              underlayColor={'#B1B8B9'}
+              style={styles.successButton}
+              value={true}
+              onPress={this.handleSubmit.bind(this)}>
+              <Text style={styles.issueType}>
+                File Another Report
+              </Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+        );
+        return layout;
+    }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 120,
+  },
+  successContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 24,
+    paddingRight: 24
+  },
+  successHeader: {
+    paddingBottom: 50,
+    alignItems: 'center'
+  },
+  successType: {
+    fontSize: 48,
+  },
+  successButton: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    backgroundColor: '#f62745',
+    padding: 16,
+    borderWidth: 1,
+    borderColor: '#ffffff'
+  },
+  issueType: {
+    color: '#ffffff',
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+});
+
+
+module.exports = SnapPicture;
