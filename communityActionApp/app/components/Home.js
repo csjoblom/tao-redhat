2c35937beac887716ef19232f678e31fa3426f1f
Bishop Lafer <bishoplafer@Bishops-MacBook-Pro.local>
2016-02-17 13:27:52 -0800

    Home.js

---
+++ communityActionApp/app/components/Home.js
@@ -0,0 +1,17 @@
+var React = require('react-native');
+var Impact = require('./Impact');
+
+var {
+    Image,
+    TouchableHighlight,
+    Text,
+    StyleSheet,
+    View
+} = React;
+
+
+
+
+const styles = StyleSheet.create({
+
+});
